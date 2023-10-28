7c0c6eb949f7ea47119048a6a7f4335d72b9dd98
Kushagra Gangwar <kgang6434@gmail.com>
2023-10-29 01:02:29 +0530

    chat app commit

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const express = require('express');
-const app= express();
+const express=require('express');
+const path=require('path');
+const { Socket } = require('socket.io');
+const app= express()
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to the home page baby, you are beautiful fool !");
+const PORT=process.env.PORT || 3000
+const server=app.listen(PORT,()=>{
+    console.log(`our chat server is running on ${PORT}`);
 });
-const port = process.env.port || 3000
-app.listen(port, ()=>{
-    console.log(`server is listening on port ${port}`);
-});
+
+const io=require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'public')));
+
+let socketConnected=new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(socket.id);
+    socketConnected.add(socket.id)
+
+    io.emit('clients-total',socketConnected.size) //to check the clients number
+
+    socket.on('disconnect',()=>{
+        console.log("Socket disconnected",socket.id);
+        socketConnected.delete(socket.id);
+        io.emit('clients-total',socketConnected.size)  // to again check after deletion
+    })
+
+    socket.on('message',(data)=>{
+        // console.log(data);
+        socket.broadcast.emit('chatmessage',data)
+    })
+
+    socket.on('feedback',(data)=>{
+        socket.broadcast.emit('feedback',data)
+    })
+}
 
 
